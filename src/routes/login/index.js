6ae02a79a5a81280f35394d85ac7bde6147cce00
marui01 <marui01@ofo.com>
2018-10-03 23:43:02 +0800

    add login

--- src/routes/login/index.js
+++ src/routes/login/index.js
@@ -4,7 +4,39 @@ import {Button, Icon, Input, Select, Timeline, message, DatePicker, Radio} from 
 import styles from './index.less'
 
 class Login extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: '',
+            password: '',
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleName = this.handleName.bind(this);
+        this.handlePass = this.handlePass.bind(this);
+    }
+    handleSubmit (e) {
+        console.log(this.state.username, "&&&&&&&&&", this.state.password);
+    }
+    handleName (e) {
+        this.setState({username: e.target.username});
+    }
+    handlePass (e) {
+        this.setState({password: e.target.password});
+    }
     render() {
-        return (<div></div>)
+        return (<div className="styles.login">
+            <h1>售前项目管理系统</h1>
+            <form onSubmit="handleSubmit">
+                <label>
+                    账户
+                    <input type="text" value={this.state.username} onChange={this.handleName}/>
+                </label>
+                <label>
+                    密码
+                    <input type="password" value={this.state.password} onChange={this.handlePass}/>
+                </label>
+                <input className="button" type="submit" value="登录" />
+            </form>
+        </div>)
     }
 }
